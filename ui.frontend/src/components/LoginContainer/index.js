c1e22bd3ebd4c90e013528fd8ff45ab92eaaf1ed
marcelostial <marceloaface@outlook.com>
2022-07-13 19:04:19 -0300

    error

---
+++ ui.frontend/src/components/LoginContainer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MapTo } from '@adobe/aem-react-editable-components';
+import './style.css'
+
+const LoginContainer = () => {
+  return (
+    <div>
+      ola
+    </div>
+  )
+}
+
+export default MapTo('reactapp/components/LoginContainer')(LoginContainer);
